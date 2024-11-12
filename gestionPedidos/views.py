56619fe8e4e1ae035344a6f9ae6e92fa2f4b319e
Robert <robert.roqueb19@gmail.com>
2024-11-11 21:11:41 -0500

    formularios

--- gestionPedidos/views.py
+++ gestionPedidos/views.py
@@ -1,3 +1,19 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from gestionPedidos.models import Articulos
+
+
 # Create your views here.
+def busqueda_productos(request):
+    return render(request, 'busqueda_productos.html')
+
+def buscar(request):
+    if request.GET['prd']:
+        #mensaje='Articulo buscado: %s' %request.GET['prd']
+        producto=request.GET['prd']
+        articulos=Articulos.objects.filter(nombre__icontains=producto)
+        return render(request,'resultados_busqueda.html',{'articulos':articulos,'query':producto})
+    else:
+        mensaje='No has introducido nada'
+    return HttpResponse(mensaje)
