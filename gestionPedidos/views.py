6a2438c74691de2c4e4f9c31b63765c8c2524ee1
Robert <robert.roqueb19@gmail.com>
2024-11-22 18:43:31 -0500

    formulario busqueda y email

--- gestionPedidos/views.py
+++ gestionPedidos/views.py
@@ -12,8 +12,16 @@ def buscar(request):
     if request.GET['prd']:
         #mensaje='Articulo buscado: %s' %request.GET['prd']
         producto=request.GET['prd']
-        articulos=Articulos.objects.filter(nombre__icontains=producto)
-        return render(request,'resultados_busqueda.html',{'articulos':articulos,'query':producto})
+        if len(producto)>20:
+            mensaje='Texto de busqueda demasiado largo'
+        else:
+            articulos=Articulos.objects.filter(nombre__icontains=producto)
+            return render(request,'resultados_busqueda.html',{'articulos':articulos,'query':producto})
     else:
         mensaje='No has introducido nada'
     return HttpResponse(mensaje)
+
+def contacto(request):
+    if request.method == 'POST':
+        return render(request,'gracias.html')
+    return render(request,'contacto.html')
